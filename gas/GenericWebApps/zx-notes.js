3c71baa84c834a33839952f2b04a392045b6bc29
zong <zong@globalblue.com>
2020-05-22 20:58:01 +0800

    update

--- gas/GenericWebApps/zx-notes.js
+++ gas/GenericWebApps/zx-notes.js
@@ -3,7 +3,10 @@ function doGet() {
     // Run the following link to view dev version (it supports hot-reload). So you don't have to keep publishing. Just need to save.
     // https://script.google.com/macros/s/AKfycbyCCaxaYjbMN9Lri8OpphIK67Ax_DMtZm8HrMl1oayQVYyY6hI/exec
     // https://script.google.com/macros/s/AKfycbzUD1buA-9AKZzXxTc9HXIzfbBSZj7TmZkFx4Zgalc/dev
-    // 
+
+    // https://script.google.com/macros/s/AKfycbyCCaxaYjbMN9Lri8OpphIK67Ax_DMtZm8HrMl1oayQVYyY6hI/exec
+    // https://script.google.com/macros/s/AKfycbzUD1buA-9AKZzXxTc9HXIzfbBSZj7TmZkFx4Zgalc/dev
+
     return ContentService.createTextOutput("Some content for embedding");
 
 
