aa4202610fa687a4f040108d79e4367d3b4aafbe
zong <zong@globalblue.com>
2020-07-08 13:48:19 +0800

    working api

--- jacon/src/main/java/jacon/App.java
+++ jacon/src/main/java/jacon/App.java
@@ -10,5 +10,12 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+
+        // CryptoTest test = new CryptoTest();
+        // test.DoWork();
+
+        ApiHelper apiHelper = new ApiHelper();
+        apiHelper.DoWork();
+
     }
 }
